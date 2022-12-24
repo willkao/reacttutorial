60eae64c1ff58bb04d47f63d1ec9685eb2d761a1
Will Kao <willkao21@gmail.com>
2022-12-23 21:25:32 -0800

    Declare winner + bug fixes
    Remove ability to click previously clicked squares, winning ends clicking

--- src/index.js
+++ src/index.js
@@ -21,6 +21,9 @@ function Square(props) {
 
     handleClick(i) {
       const squares = this.state.squares.slice();
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
       squares[i] = this.state.xIsNext?'X':'O';
       this.setState({
         squares: squares,
@@ -33,7 +36,13 @@ function Square(props) {
     }
   
     render() {
-      const status = 'Next player: ' + (this.state.xIsNext?'X':'O');
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
   
       return (
         <div>
@@ -93,7 +102,7 @@ function Square(props) {
     }
     return null;
   }
-  
+
   // ========================================
   
   const root = ReactDOM.createRoot(document.getElementById("root"));
